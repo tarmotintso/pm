2a910ad93b351046e8dc4771eb0a174ab56604a2
HenriLindepuu <henri.lindepuu@khk.ee>
2014-04-29 10:54:33 +0300

    Add postgresql support

--- config.sample.php
+++ config.sample.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DATABASE_HOSTNAME', '127.0.0.1');
-define('DATABASE_USERNAME', 'root');
+define('DATABASE_USERNAME', 'postgres');
 define('DATABASE_PASSWORD', '');
-define('DATABASE_DATABASE', PROJECT_NAME);
+define('DATABASE_DATABASE', 'piletimaailm_prod');
