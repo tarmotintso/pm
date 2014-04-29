4ebc9ae163cea7dbd8faf5100c5133a19f2f497b
Henno Täht <henno.taht@gmail.com>
2014-04-29 15:46:43 +0300

    Change to _dev database by default

--- config.sample.php
+++ config.sample.php
@@ -3,4 +3,4 @@
 define('DATABASE_HOSTNAME', '127.0.0.1');
 define('DATABASE_USERNAME', 'postgres');
 define('DATABASE_PASSWORD', '');
-define('DATABASE_DATABASE', 'piletimaailm_prod');
+define('DATABASE_DATABASE', 'piletimaailm_dev');
