ee6f0251802394441f4a1e4328cd11d38f127fe3
Aya <aet.prass@khk.ee>
2014-04-29 16:13:09 +0300

    Resolve issue with events not displaying on main page

--- controllers/newsarticles.php
+++ controllers/newsarticles.php
@@ -5,7 +5,7 @@ class newsarticles extends Controller
 
     function index()
     {
-        $this->events = get_all("SELECT * FROM small_piletimaailm.events WHERE type='Event' and state='on_sale'");
+        $this->events = get_all("SELECT * FROM events WHERE type='Event' and state='on_sale'");
     }
 
 }
